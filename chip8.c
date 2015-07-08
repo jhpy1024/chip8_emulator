d9868283815cb4c9bacc897ee91c141d1bf96507
jhpy1024 <jhpy1024@gmail.com>
2015-07-08 14:03:22 +0100

    Added comments giving meanings of opcodes

--- chip8.c
+++ chip8.c
@@ -122,81 +122,154 @@ void chip8_execute_opcode(chip8* c)
     (chip8_ops[(c->current_opcode & 0xF000) >> 12])(c);
 }
 
+/* 
+ * 0NNN: Calls RCA 1802 program at address NNN.
+ * 00E0: Clears the screen.
+ * 00EE: Returns from a subroutine.
+ */
 void chip8_op_0(chip8* c)
 {
 
 }
 
+/*
+ * 1NNN: Jumps to address NNN.
+ */
 void chip8_op_1(chip8* c)
 {
 
 }
 
+/*
+ * 2NNN: Calls subroutine at NNN.
+ */
 void chip8_op_2(chip8* c)
 {
 
 }
 
+/*
+ * 3XNN: Skips the next instruction if VX equals NN.
+ */
 void chip8_op_3(chip8* c)
 {
 
 }
 
+/*
+ * 4XNN: Skips the next instruction if VX doesn't equal NN.
+ */
 void chip8_op_4(chip8* c)
 {
 
 }
 
+/*
+ * 5XY0: Skips the next instruction if VX equals VY.
+ */
 void chip8_op_5(chip8* c)
 {
 
 }
 
+/*
+ * 6XNN: Sets VX to NN.
+ */
 void chip8_op_6(chip8* c)
 {
 
 }
 
+/*
+ * 7XNN: Adds NN to VX.
+ */
 void chip8_op_7(chip8* c)
 {
 
 }
 
+/*
+ * 8XY0: Sets VX to the value of VY.
+ * 8XY1: Sets VX to VX or VY.
+ * 8XY2: Sets VX to VX and VY.
+ * 8XY3: Sets VX to VX xor VY.
+ * 8XY4: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
+ * 8XY5: VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+ * 8XY6: Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift.
+ * 8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+ * 8XYE: Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift.
+ */
 void chip8_op_8(chip8* c)
 {
 
 }
 
+/*
+ * 9XY0: Skips the next instruction if VX doesn't equal VY.
+ */
 void chip8_op_9(chip8* c)
 {
 
 }
 
+/*
+ * ANNN: Sets I to the address NNN.
+ */
 void chip8_op_a(chip8* c)
 {
 
 }
 
+/*
+ * BNNN: Jumps to the address NNN plus V0.
+ */
 void chip8_op_b(chip8* c)
 {
 
 }
 
+/*
+ * CXNN: Sets VX to a random number, masked by NN.
+ */
 void chip8_op_c(chip8* c)
 {
 
 }
 
+/*
+ * DXYN: Sprites stored in memory at location in index register (I), maximum 8bits wide.
+ *       Wraps around the screen. If when drawn, clears a pixel, register VF is set to 1 otherwise it is zero.
+ *       All drawing is XOR drawing (i.e. it toggles the screen pixels)
+ */
 void chip8_op_d(chip8* c)
 {
 
 }
 
+/*
+ * EX9E: Skips the next instruction if the key stored in VX is pressed.
+ * EXA1: Skips the next instruction if the key stored in VX isn't pressed.
+ */
 void chip8_op_e(chip8* c)
 {
 
 }
 
+/*
+ * FX07: Sets VX to the value of the delay timer.
+ * FX0A: A key press is awaited, and then stored in VX.
+ * FX15: Sets the delay timer to VX.
+ * FX18: Sets the sound timer to VX.
+ * FX1E: Adds VX to I.
+ * FX29: Sets I to the location of the sprite for the character in VX.
+ *       Characters 0-F (in hexadecimal) are represented by a 4x5 font.
+ * FX33: Stores the Binary-coded decimal representation of VX, with the most significant
+ *       of three digits at the address in I, the middle digit at I plus 1, and the least significant
+ *       digit at I plus 2. (In other words, take the decimal representation of VX, place the hundreds
+ *       digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.)
+ * FX55: Stores V0 to VX in memory starting at address I.
+ * FX65: Fills V0 to VX with values from memory starting at address I.
+ */
 void chip8_op_f(chip8* c)
 {
 
@@ -219,47 +292,3 @@ void chip8_update_timers(chip8* c)
         c->sound_timer--;
     }
 }
-
-/*
-void chip8_op_set_address(chip8* c)
-{
-    c->address_register = c->current_opcode & 0x0FFF;
-    c->program_counter += 2;
-}
-*/
-
-/*
-void chip8_op_clear_screen(chip8* c)
-{
-    chip8_clear_graphics(c);
-}
-*/
-
-/*
-void chip8_op_call_sub(chip8* c)
-{
-    c->stack[c->stack_pointer++] = c->program_counter;
-    c->program_counter = c->current_opcode & 0x0FFF;
-}
-*/
-
-/*
-void chip8_op_jump(chip8* c)
-{
-    c->program_counter = c->current_opcode & 0x0FFF;
-}
-*/
-
-/*
-void chip8_op_add_y_to_x(chip8* c)
-{
-    int x_index = (c->current_opcode & 0x0F00) >> 8;
-    int y_index = (c->current_opcode & 0x00F0) >> 4;
-    unsigned char x = c->registers[x_index];
-    unsigned char y = c->registers[y_index];
-
-    c->registers[0xF] = y > (0xFF - x) ? 1 : 0;
-    c->registers[x_index] += c->registers[y_index];
-    c->program_counter += 2;
-}
-*/
